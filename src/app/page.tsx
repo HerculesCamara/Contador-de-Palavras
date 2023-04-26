be8f264024b31e5023fb08d889cb61925a7a8e40
Hercules Camara <herculescamara2013@gmail.com>
2023-04-26 13:17:00 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,51 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const calculateWordCount = () => {
+        const words = text.trim().split(" ");
+        setWordCount(words.filter((word) => word !== "").length);
+      };
+
+      calculateWordCount();
+
+      const intervalId = setInterval(calculateWordCount, 1000);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [text]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-center p-5 md:p-x-24">
+      <div className="relative flex items-center justify-center w-auto">
+        {/* <span className="absolute text-green-500 font-bold text-5xl mb-5 font-body text-center -top-3 text-transparent drop-shadow-sm md:filter-none">Contador&nbsp;de&nbsp;Palavras</span> */}
+        <h1 className="text-green-500 font-bold text-4xl mb-5 font-body text-center">Contador de Palavras</h1>
+      </div>
+
+      <textarea
+        value={text}
+        onChange={handleChange}
+        rows="15"
+        placeholder="Escreva aqui e veja quantas palavras seu texto tem"
+        className="h-80 md:h-auto rounded-2xl p-4 md:p-8 text-xs md:text-xl text-neutral-200 bg-neutral-700 backdrop-opacity-10 backdrop-invert border-green-800 border w-5/6"
+      />
+      <p className="text-gray-200 font-bold font-body text-2xl mt-5">
+        Total: <span className="text-green-400">{wordCount} palavras</span>
+      </p>
+    </main>
+  );
+}
+
+
+{/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -118,7 +157,4 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
-    </main>
-  )
-}
+      </div> */}
